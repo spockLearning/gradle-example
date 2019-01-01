8fb6468079e0355a765998c385aa529e21ce2a54
sqamonkey <32463711+sqamonkey@users.noreply.github.com>
2018-12-31 22:43:26 -0700

    Add files via upload

---
+++ src/test/groovy/BasicTest.groovy
@@ -0,0 +1,14 @@
+import spock.lang.Specification
+
+class BasicTest extends Specification {
+
+    def "this does something"() {
+        given:
+
+        when:
+        // TODO: add when
+
+        then:
+        true
+    }
+}
